bc92952baa7f840a35f59f662a73b94d58e5f6dc
vpetridis <vasileios.petridis@gmail.com>
2021-01-09 22:35:31 +0200

    created main built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Item from "./Item";
 import imageData from "./ImageData";
+import ItemList from "./ItemList";
 
 export default function App() {
-    const allItems = imageData.map((image) => 
-     <Item key={image.id} imageData={image} />
+    const slicedItems = imageData.map((image) => (
+        <Item key={image.id} imageData={image} />
+    )).slice(2,4)
+    const style={h1:{color:"blue"}}
+    return (
+      <div>
+            <ItemList styles={{color:"green"}}/>
+            {slicedItems}
+
+        </div>
     );
-    const slicedItems = allItems.slice(2,4);
-    return <div>{slicedItems} </div>;
 }
---
+++ src/ItemList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class ItemList extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+                <h1 style={this.props.styles}>testing class components</h1>
+
+
+            </div>
+        )
+    }
+
+
+}
+
+export default  ItemList
