74975be2c4ce3e0a6eaa180e81447ff23bb140e4
vpetridis <vasileios.petridis@gmail.com>
2021-01-09 13:22:30 +0200

    fixed positioning

--- src/Item.js
+++ src/Item.js
@@ -4,7 +4,7 @@ export default function Item(props) {
     const { thumbnailUrl, id, title, url } = props.imageData;
 
     return (
-        <div>
+        <div className="item-container">
             <img src={thumbnailUrl} alt="" />
             <h1>{title}</h1>
             <a href={url}>Load more</a>
