403510c92fbab3cafb968a249bc879521649abe6
vpetridis <vasileios.petridis@gmail.com>
2021-01-09 22:48:34 +0200

    deleted all cleaned proj

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import Item from "./Item";
 import imageData from "./ImageData";
-import ItemList from "./ItemList";
 
 export default function App() {
-    const slicedItems = imageData.map((image) => (
-        <Item key={image.id} imageData={image} />
-    )).slice(2,4)
-    const style={h1:{color:"blue"}}
-    return (
-      <div>
-            <ItemList styles={{color:"green"}}/>
-            {slicedItems}
+    // const slicedItems = imageData
+    //     .map((image) => <Item key={image.id} imageData={image} />)
+    //     .slice(2, 50);
 
+    return (
+        <div >
         </div>
     );
 }
